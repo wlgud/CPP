a9e373019d385c3d83872dffe9aa9f543d3d1f20
wlgud <pjh82381425@gmail.com>
2026-03-17 17:28:48 +0900

    p97_2

--- Week3/p97_1.cpp
+++ Week3/p97_1.cpp
@@ -1,3 +1,5 @@
+// 1-1
+
 #include <iostream>
 
 int main()
@@ -12,5 +14,72 @@ int main()
         }
 
         std::cout << "\n";
+    }
+}
+
+//1-2
+
+#include <iostream>
+
+int main()
+{
+    for(int i = 1; i <= 5; ++i)
+    {
+        for(int j = 5; j >= i; --j)
+        {
+            std::cout << j;
+        }
+
+        std::cout << "\n";
+    }
+}
+
+//1-3
+
+#include <iostream>
+
+int main()
+{
+    for(int i = 1; i <= 5; ++i)
+    {
+        for(int j = 1; j <= 5 - i; ++j)
+        {
+            std::cout << " ";
+        }
+
+        for (int j = i; j >= 1; --j)
+        {
+            std::cout << j;
+        }
+       
+        std::cout << "\n";
+    }
+}
+
+//1-4
+
+#include <iostream>
+
+int main()
+{
+    for(int i = 1; i <= 5; ++i)
+    {
+
+       for(int j = 1; j <= 5 - i; ++j)
+      {
+         std::cout << " ";
+      }
+
+     for(int j = i; j >= 1; --j)
+     {
+         std::cout << j;
+     }
+
+    for(int j = 2; j <= i; ++j)
+    {
+        std::cout << j;
+    }
+
+     std::cout << "\n";
     }
 }
